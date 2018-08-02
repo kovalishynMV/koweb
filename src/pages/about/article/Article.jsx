a6ea36265c441885637eb35e38088c136558d537
kovalishynmv <kovalishynmv@gmail.com>
2018-08-03 00:13:41 +0300

    Готовая страница about

--- src/pages/about/article/Article.jsx
+++ src/pages/about/article/Article.jsx
@@ -5,7 +5,29 @@ class Article extends React.Component {
     render() {
         return (
             <div className="article">
+                <div className="article__left">
+                    <h5>
+                        ABOUT
+                    </h5>
 
+                    <div className="article__lines" />
+
+                    <h1>
+                        Kovalishyn Maksym
+                    </h1>
+
+                </div>
+                <div className="article__right">
+                    <p>
+                    I am an innovative award-winning UI / UX Designer with more than 3 years of commercial experience in big companies with more than 200 employees and small startups. I have a passion for new design techniques and methodologies and also I am a winner of DevChallenge12 in Pro Web-Design Category.
+                    <p>&nbsp;</p>
+
+As UI/UX Designer, I have experience in creating an entirely new style guide from scratch (color selection, typography etc.) and using the existing one, creating of customer journey maps, user personas, information architectures and so on.
+<p>&nbsp;</p>
+
+We will make your product perfect together!
+                    </p>
+                </div>
             </div>
         );
     }
--- src/pages/about/footer/Footer.jsx
+++ src/pages/about/footer/Footer.jsx
@@ -5,7 +5,24 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footer">
+                <ul class="footer__menu_left">
+                    <li><a href="">WORK</a></li>
+                    <li><a href="">ABOUT</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
 
+                <ul class="footer__menu_right">
+                    <li><a href="">DRIBBBLE</a></li>
+                    <li><a href="">BEHANCE</a></li>
+                    <li><a href="">INSTAGRAM</a></li>
+                    <li><a href="">LINKEDIN</a></li>
+                </ul>
+
+                <div className="footer__caption">
+                    <caption>
+                        © 2018 Maksym Kovalishyn. Ukrainian UI/UX Designer
+                    </caption>
+                </div>
             </div>
         );
     }
