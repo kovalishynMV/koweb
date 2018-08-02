e7addbb55c2c12a8916debc96d27415b7dc28bc8
kovalishynmv <kovalishynmv@gmail.com>
2018-08-02 16:50:06 +0300

    4

--- src/pages/about/header/Header.jsx
+++ src/pages/about/header/Header.jsx
@@ -6,7 +6,16 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <div className="logo" />
+                <div className="logo">
+                    
+                </div>
+
+                <ul class="header__menu">
+                    <li><a href="">WORK</a></li>
+                    <li><a href="">ABOUT</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+
             </header>
         );
     }
