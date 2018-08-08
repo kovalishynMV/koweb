50ec8e946bfc82c99b1e68637b8a42ba689a758b
kovalishynmv <kovalishynmv@gmail.com>
2018-08-08 15:43:23 +0300

    коммит до создания роута

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <body className="contact-page">
+
+            </body>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/pages/work/Work.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Work extends React.Component {
+    render() {
+        return (
+            <body className="work-page">
+
+            </body>
+        );
+    }
+}
+
+export default Work;
