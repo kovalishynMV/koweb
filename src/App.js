071f3d3b1fbb213350c374e9f80b3e3c90c132c0
kovalishynmv <kovalishynmv@gmail.com>
2018-08-09 17:50:10 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Work from "./pages/work/Work"
 import About from "./pages/about/About"
 import Contact from "./pages/contact/Contact"
--- src/pages/contact/contact-form/ContactForm.jsx
+++ src/pages/contact/contact-form/ContactForm.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import './contact-form.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const styles = {
+    root: {
+      
+    },
+    input: {
+      
+    }
+  };
 
 class ContactForm extends React.Component {
     render() {
@@ -13,13 +26,40 @@ class ContactForm extends React.Component {
                     Always in touch!
                 </h1>
 
-                <p>
+                <p max-width="300px">
                     If you have any questions or you just want to say "Hello!", Write to me
                 </p>
+                
+                <div className="contact-form__inputs">
 
+                    <TextField
+                        id="with-placeholder"
+                        label="Name"
+                        margin="normal"
+                    />
+
+                    <TextField
+                        id="with-placeholder"
+                        label="E-mail"
+                        margin="normal"
+                    />
+
+                    <TextField
+                        id="multiline-flexible"
+                        label="Message"
+                        multiline
+                        rowsMax="4"
+                        margin="normal"
+                    />
+
+                    <Button id="button">
+                        SEND MESSAGE
+                    </Button>
+
+                </div>
             </div>
         );
     }
 }
 
-export default ContactForm;
+export default withStyles(styles)(ContactForm);
