02a2bcb89103a78e51806723f2865efdc1584383
kovalishynmv <kovalishynmv@gmail.com>
2018-08-22 21:04:22 +0300

    Коммит перед валидацией формы

--- src/pages/contact/contact-form/ContactForm.jsx
+++ src/pages/contact/contact-form/ContactForm.jsx
@@ -6,26 +6,55 @@ import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import { createMuiTheme } from '@material-ui/core/styles';
 
-const THEME = createMuiTheme({
-    typography: {
-     "fontFamily": "Raleway",
-     "fontWeightLight": 300,
-     "fontWeightRegular": 400,
-     "fontWeightMedium": 500
-    }
- });
+const styles = {
+    button: {
+        border: 'solid 1px #172bff',
+        borderRadius: '2px',
+        marginTop: '50px',
+        width: '300px',
+        height: '54px',
+        fontFamily: 'Raleway',
+        fontSize: '12px',
+        fontWeight: '600',
+        fontStyle: 'normal',
+        fontStretch: 'normal',
+        lineHeight: '1.5',
+        letterSpacing: '2px',
+        textAlign: 'center',
+        color: '#172bff',
 
-const styles = theme => ({
-    textField: {
+        '&:hover': {
+            backgroundColor: '#172bff',
+            boxShadow: '0 15px 35px 0 rgba(23, 43, 255, 0.12)',
+            color: '#fff',
+            transition: 'all 180ms',
+          },
+
+        '&:disabled': {
+            backgroundColor: 'rgba(231, 232, 234, 0.5)',
+            color:'#b6b9c8',
+            border: '0px',
+        }
     },
-  });
-  
 
-class ContactForm extends React.Component {
+    TextField: {
+        fontFamily: 'Raleway',
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontStretch: 'normal',
+        lineHeight: '1.5',
+        letterSpacing: 'normal',
+        color: '#b6b9c8',
 
-    render() {
+        '&:after': {
+            backgroundColor: '#fff',
+        }
+    }
+  };
 
-        const {classes} = this.props;
+  function ContactForm(props) {
+
 
         return (
             
@@ -41,47 +70,41 @@ class ContactForm extends React.Component {
                 <p span class="contact-hello">
                     If you have any questions or you just want to say "Hello!", Write to me
                 </p>
-                
-                <div className="contact-form__inputs">
+                    <form className="contact-form__inputs">
+                        <TextField
+                            className={props.classes.TextField}
+                            id="with-placeholder"
+                            label="Name"
+                            margin="normal">
+                        </TextField>
 
-                    <TextField 
-                        classes={{
-                            root: classes.root,
-                            label: classes.label,
-                            underline: classes.underline,
-                        }}
-                        id="with-placeholder"
-                        label="Name"
-                        margin="normal"
-                    />
+                        <TextField
+                            className={props.classes.TextField}
+                            id="with-placeholder"
+                            label="E-mail" 
+                            argin="normal">
+                        </TextField>
 
-                    <TextField
-                        id="with-placeholder"
-                        label="E-mail"
-                        margin="normal"
-                        className="classes.textField"
-                    />
+                        <TextField
+                            className={props.classes.TextField}
+                            id="multiline-flexible"
+                            label="Message"
+                            multiline
+                            rowsMax="8"
+                            margin="normal">
+                        </TextField>
 
-                    <TextField
-                        id="multiline-flexible"
-                        label="Message"
-                        multiline
-                        rowsMax="8"
-                        margin="normal"
-                    />
-
-                    <Button id="button">
-                        SEND MESSAGE
-                    </Button>
-
+                        <Button disabled className={props.classes.button} >
+                            {props.children ? props.children : 'SEND MESSAGE'}
+                        </Button>
+                    </form>
                 </div>
-            </div>
         );
     }
-}
 
 ContactForm.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+    children: PropTypes.node,
+    classes: PropTypes.object.isRequired,
+  };
 
 export default withStyles(styles)(ContactForm);
